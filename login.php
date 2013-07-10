e6cf6af1ab8f782591df68cc86211967f4a1122e
hagay bauman <haji_bauman@hotmail.com>
2013-07-11 01:09:29 +0300

    updating

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>welcome to porjectX</h1>
+	<h1>login page</h1>
+
+	<p>מלא פרטים וכנס למערכת:</p>
+	<form method="post" action="update.php">
+		user Name: 	<input type="text" name="user"/><br/>
+		password: 	<input type="text" name="pass"/><br/>
+		<input type="submit" name="submit"/>
+</body>
+	<p>	משתמש חדש? <a href="http://localhost/ourProject/ourProject/testing.php">לחץ כאן</a></p>	
+	</form>
+	
